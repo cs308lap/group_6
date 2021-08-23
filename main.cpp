730f9346f7c064400e157b87d9ed4c36bed66008
binti-p <padaliya.binti@gmail.com>
2021-08-23 15:03:29 +0530

    printf to cout

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     print_hello();
-    printf("\n The factorial of 5 is %d\n",factorial(5));
-    // cout<<"The factorial of 5 is "<< factorial(5)<< endl;
+    // printf("\n The factorial of 5 is %d\n",factorial(5));
+    cout<<"The factorial of 5 is "<< factorial(5)<< endl;
     return 0;
 }
